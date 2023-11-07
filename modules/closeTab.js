1b101744ea7ac47d72af6fbc31b2bc5346b0d1c9
AidasD <ntaidas@gmail.com>
2023-11-07 20:02:46 +0200

    added transition when tab opens and closes

--- modules/closeTab.js
+++ modules/closeTab.js
@@ -1,5 +1,8 @@
 export default function closeTab(tab){
     document.querySelector('main').style.display='flex';
-    tab.remove();
+    tab.classList.add('hide');
     document.querySelector('html').style.height = '100%';
+    setTimeout(()=>{    tab.remove();
+        document.querySelector("main").classList.remove('hide');},500)
+
 }
