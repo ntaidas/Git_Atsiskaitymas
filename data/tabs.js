25e1d79e39df7ab6118b71af6adec71f58acb2ec
AidasD <ntaidas@gmail.com>
2023-11-07 18:50:06 +0200

    added functionality to class to be able to add videos

--- data/tabs.js
+++ data/tabs.js
@@ -1,6 +1,6 @@
 const intro = {
   header: "Intro",
-  img: "../pictures/tab-1.mp4",
+  video: "../pictures/tab-1.mp4",
   paragraphs: [
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore nisi fugiat ratione nulla iusto, quibusdam sequi ducimus amet doloremque porro quaerat ex fugit totam minima. Consequuntur quod temporibus laudantium.",
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore nisi fugiat ratione nulla iusto, quibusdam sequi ducimus amet doloremque porro quaerat ex fugit totam minima. Consequuntur quod temporibus laudantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius mollitia impedit hic nemo! Ipsum facilis alias maiores harum in dignissimos tempora. Placeat quos recusandae voluptatum possimus, error animi eligendi dignissimos.",
