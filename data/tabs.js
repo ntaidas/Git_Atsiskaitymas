49677651c869400b227a3129827aa13063ae91ee
AidasD <ntaidas@gmail.com>
2023-11-07 20:27:47 +0200

    checking why img dont appear in github

--- data/tabs.js
+++ data/tabs.js
@@ -1,6 +1,6 @@
 const intro = {
   header: "Intro",
-  video: "../pictures/tab1.mp4",
+  video: "./pictures/tab1.mp4",
   paragraphs: [
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore nisi fugiat ratione nulla iusto, quibusdam sequi ducimus amet doloremque porro quaerat ex fugit totam minima. Consequuntur quod temporibus laudantium.",
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore nisi fugiat ratione nulla iusto, quibusdam sequi ducimus amet doloremque porro quaerat ex fugit totam minima. Consequuntur quod temporibus laudantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius mollitia impedit hic nemo! Ipsum facilis alias maiores harum in dignissimos tempora. Placeat quos recusandae voluptatum possimus, error animi eligendi dignissimos.",
@@ -9,7 +9,7 @@ const intro = {
 
 const work = {
   header: "Work",
-  img: "../pictures/tab2.jpg",
+  img: "./pictures/tab2.jpg",
   paragraphs: [
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore nisi fugiat ratione nulla iusto, quibusdam sequi ducimus amet doloremque porro quaerat ex fugit totam minima. Consequuntur quod temporibus laudantium.",
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore nisi fugiat ratione nulla iusto, quibusdam sequi ducimus amet doloremque porro quaerat ex fugit totam minima. Consequuntur quod temporibus laudantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius mollitia impedit hic nemo! Ipsum facilis alias maiores harum in dignissimos tempora. Placeat quos recusandae voluptatum possimus, error animi eligendi dignissimos.",
@@ -18,7 +18,7 @@ const work = {
 
 const about = {
   header: "About",
-  img: "../pictures/tab3.jpg",
+  img: "./pictures/tab3.jpg",
   paragraphs: [
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore nisi fugiat ratione nulla iusto, quibusdam sequi ducimus amet doloremque porro quaerat ex fugit totam minima. Consequuntur quod temporibus laudantium.",
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore nisi fugiat ratione nulla iusto, quibusdam sequi ducimus amet doloremque porro quaerat ex fugit totam minima. Consequuntur quod temporibus laudantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius mollitia impedit hic nemo! Ipsum facilis alias maiores harum in dignissimos tempora. Placeat quos recusandae voluptatum possimus, error animi eligendi dignissimos.",
@@ -27,7 +27,7 @@ const about = {
 
 const contact = {
   header: "Contact",
-  img: "../pictures/tab4.jpg",
+  img: "./pictures/tab4.jpg",
   paragraphs: [
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore nisi fugiat ratione nulla iusto, quibusdam sequi ducimus amet doloremque porro quaerat ex fugit totam minima. Consequuntur quod temporibus laudantium.",
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore nisi fugiat ratione nulla iusto, quibusdam sequi ducimus amet doloremque porro quaerat ex fugit totam minima. Consequuntur quod temporibus laudantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius mollitia impedit hic nemo! Ipsum facilis alias maiores harum in dignissimos tempora. Placeat quos recusandae voluptatum possimus, error animi eligendi dignissimos.",
