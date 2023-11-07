4dee9d9a553513a5eba254dd6289c47bb1f1a51a
AidasD <ntaidas@gmail.com>
2023-11-07 16:57:04 +0200

    tabs.js:paragraph changed to paragaphs, finished Tab class , intro button working

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+import { intro } from "./data/tabs.js";
 import Tab from "./modules/tabClass.js";
 
 document.querySelector('#intro').addEventListener('click',()=>{
     document.querySelector('main').style.display='none';
-    const tab = new Tab;
+    const tab = new Tab(intro);
 })
--- modules/tabClass.js
+++ modules/tabClass.js
@@ -1,9 +1,10 @@
 import closeTab from "./closeTab.js";
 
 export default class Tab {
-  constructor() {
-    // this.img = img;
-    // this.paragrapgh = paragrapgh;
+  constructor({header,img,paragraphs}) {
+     this.header = header;
+     this.img = img;
+     this.paragraphs = paragraphs;
     return this.display();
   }
   display() {
@@ -14,12 +15,24 @@ export default class Tab {
     const closeImg = document.createElement("img");
     closeImg.setAttribute("src", "../pictures/xmark-solid.svg");
     closeButton.append(closeImg);
-    tab.append(closeButton);
     document.querySelector("body").append(tab);
     closeButton.addEventListener("click", () => closeTab(tab));
-
-
-
+    
+    const header = document.createElement('h1');
+    const headerTxt = document.createTextNode(this.header);
+    header.appendChild(headerTxt);
+    const coverImg = document.createElement('img');
+    coverImg.setAttribute('src',this.img);
+    coverImg.setAttribute('alt',this.header);
+    tab.append(closeButton,header,coverImg);
+    this.paragraphs.forEach(paragraph => {
+        console.dir(paragraph)
+        const p = document.createElement('p');
+        const pTxt = document.createTextNode(paragraph);
+        p.append(pTxt)
+        tab.append(p)
+    })
+    
     return tab
   }
 }
