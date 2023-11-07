9afc5cd3cae9dab06bb4bbc976beab40123321ff
AidasD <ntaidas@gmail.com>
2023-11-07 17:02:17 +0200

    added event listener on all nav elements turning them into buttons

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-import { intro } from "./data/tabs.js";
+import { about, contact, intro, work } from "./data/tabs.js";
 import Tab from "./modules/tabClass.js";
 
 document.querySelector('#intro').addEventListener('click',()=>{
     document.querySelector('main').style.display='none';
     const tab = new Tab(intro);
+})
+document.querySelector('#work').addEventListener('click',()=>{
+    document.querySelector('main').style.display='none';
+    const tab = new Tab(work);
+})
+document.querySelector('#about').addEventListener('click',()=>{
+    document.querySelector('main').style.display='none';
+    const tab = new Tab(about);
+})
+document.querySelector('#contact').addEventListener('click',()=>{
+    document.querySelector('main').style.display='none';
+    const tab = new Tab(contact);
 })
